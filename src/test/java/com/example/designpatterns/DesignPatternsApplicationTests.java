e10b5a34a6c349838ab956f929465ecd51e0d5a1
Kerem Adali <kerem.adali@deepnetwork.com>
2024-03-02 17:16:35 +0300

    Initilize Spring Boot Project

---
+++ src/test/java/com/example/designpatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DesignPatternsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
